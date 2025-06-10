3576313b3ea987fd47aabf9e7ae04970b9d46866
kelvin0126-MAX <kelvinbilju456@gmail.com>
2025-06-10 21:05:20 +0000

    read

---
+++ routes/products.js
@@ -0,0 +1,27 @@
+import express from 'express';
+const router = express.Router();
+
+const products = [
+    {
+        ID:1,
+        ProductName: 'Jeerakasala Rice',
+        Category: 'Rice',
+        Quantity: '10 kg',
+        Price: '$20',
+        Description: 'It is prized for its delicate flavor,fragrant aroma, and soft, fluffy texture when cooked.',
+    },
+    {
+        ID:2,
+        ProductName: 'Cut Mango pickle',
+        Category: 'Pickle',
+        Quantity: '50 kg',
+        Price: '$10',
+        Description: 'It offers a delightful combination of sweet, tangy, and spicy flavors, making it a versatile condiment for various dishes.', 
+    }
+];
+
+router.get('/', (req,res) => {
+    res.send(products);
+});
+
+export default router

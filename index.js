ef392f5e88ac6b8fb830a263a603e61dab6fd895
kelvin0126-MAX <kelvinbilju456@gmail.com>
2025-06-10 20:09:55 +0000

    Installed express and display

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+
+const app = express();
+const port = 5000;
+
+app.use(bodyParser.json());
+
+app.get('/',(req,res)=>{
+    res.send('Hello,my friend')
+});
+app.listen(port, () => console.log(`server is running on ${port}`));
